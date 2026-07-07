c39e86b44766545d669819ab1a4796e0421c0796
HenrietteIraguha <h.iraguha@alustudent.com>
2026-07-07 14:50:13 +0000

    updating  sales_invoice_list.js to work with OBR Invoice Submission

--- burundi_compliance/burundi_compliance/client_scripts/sales_invoice_list.js
+++ burundi_compliance/burundi_compliance/client_scripts/sales_invoice_list.js
@@ -1,1 +1,29 @@
-// Sales Invoice list customizations removed
+frappe.listview_settings['OBR Invoice Submission'] = {
+  onload: function (listview) {
+    listview.page.add_action_item(__('Submit to EBMS'), function () {
+      submit_bulk_obr_submissions(listview)
+    })
+  }
+}
+
+function submit_bulk_obr_submissions(listview) {
+  let names = []
+  $.each(listview.get_checked_items(), function (key, value) {
+    names.push(value.name)
+  })
+
+  if (names.length === 0) {
+    frappe.throw(__('No rows selected.'))
+  }
+
+  frappe.call({
+    method:
+      'burundi_compliance.burundi_compliance.apis.sales_invoice.bulk_submit_invoices_to_obr',
+    args: {
+      doctype: 'OBR Invoice Submission',
+      invoice_list: names,
+    },
+    freeze: true,
+    freeze_message: __('Submitting to EBMS...'),
+  })
+}
